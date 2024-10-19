179bf3eba30fac7c7c488bc67ea1ae9b4e489aed
Mr. SxR <168710908+Mr-SxR@users.noreply.github.com>
2024-10-20 00:31:13 +0600

    Update SxR_DUMP.py

--- SxR_DUMP.py
+++ SxR_DUMP.py
@@ -1,1 +1,5 @@
-import SxR_DMP
+import platform,os
+os.system("clear")
+sxr = platform.architecture()[0]
+if "64" in sxr:import SxR_DMP
+else:print(f"The SxR tool has not yet been released for {sxr} It may be available for all bit versions soon.")
